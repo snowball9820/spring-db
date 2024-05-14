69d14fad353ceec078d7e2af541e5ce3c03e92ef
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-05-14 21:45:59 +0900

    Test: [#2] MemberRepositoryV0Test에 CRUD 테스트 추가
    - MemberRepositoryV0Test 클래스에 findById 메서드를 테스트하는 케이스를 추가함
    - `@Slf4j` 어노테이션을 추가하고, `findById` 메서드 실행 후의 결과를 로그로 기록하는 기능 추가함

--- src/test/java/hellodb/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hellodb/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,18 +1,24 @@
 package hellodb.jdbc.repository;
 
 import hellodb.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
-
+@Slf4j //로그찍자
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
+        //save
         Member member = new Member("memberV0",10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}",findMember);
     }
 }
